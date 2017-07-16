dad31dcdaeb25afc3e42cd6eb81bbec9fc194a8c
Todd Horner <tohorner@gmail.com>
2017-07-16 14:14:57 -0400

    added readme, fixed js data filpath, added gitignore

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   $.ajax({
     type: "GET",
-    url: "assets/data/Óscrcrd_data.csv",
+    url: "../data/Óscrcrd_data.csv",
     dataType: "text",
     success: function(data) {
       scrcrd_data = populateTable(data);
---
+++ readme.md
@@ -0,0 +1,4 @@
+## American Farmland Trust
+### Planning and Zoning Scorecard Research
+
+Go to [tohorner.github.io/AFT] for more info.
